c55811f1ab92a4d6c9e4d2f00ae151e47dfb5614
fespinosa <fjevictoriano@yahoo.com>
2021-08-01 07:10:06 -0400

    Initial commit

---
+++ src/main/java/dev/fespinosa/bootjsf/BootJsfApplication.java
@@ -0,0 +1,13 @@
+package dev.fespinosa.bootjsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJsfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootJsfApplication.class, args);
+    }
+
+}
